551707eb2039b08a59db8ebbd404632ad4de5bb6
Abbas-48 <ghulam.abbas@aiforlife.ai>
2024-12-11 21:09:34 +0500

    no

--- main.py
+++ main.py
@@ -13,6 +13,3 @@ app = FastAPI()
 app.include_router(books_router, prefix="/books", tags=["Books"])
 app.include_router(authors_router, prefix="/authors", tags=["Authors"])
 
-print("Hello")
-
-print("Second")
